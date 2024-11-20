b845a590f5b641892859c10b2312a62b3080b3cd
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2024-11-20 12:17:50 -0300

    Aula 02 - Criando e Estruturando a API

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 import express from "express";
 
+const posts = [
+    { 
+        descricao: "Uma foto teste", 
+        imagem: "https://placecats.com/millie/300/150", 
+        id: 1 
+    },
+    { 
+        descricao: "Gato fofo dormindo", 
+        imagem: "https://placecats.com/millie/300/150", 
+        id: 2 
+    },
+    { 
+        descricao: "Gato fazendo panqueca", 
+        imagem: "https://placecats.com/millie/300/150", 
+        id: 3 
+    },
+];
+
 const app = express();
+app.use(express.json()); //falando para o express que ele vai usar json
+
+//ouvindo a requisição
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas à Imersão!");
-})
+//url para acessar a requisição
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPost(ID){
+    return posts.findIndex((post) => {
+        return post.id === Number(ID)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPost(req.params.id);
+    res.status(200).json(posts[index]);
+});
