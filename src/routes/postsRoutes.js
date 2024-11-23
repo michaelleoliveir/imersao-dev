75524fe6587bc5a10f98321d624d8e7379fa0724
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2024-11-22 21:42:52 -0300

    Aula 05 - Configuração da API

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from "express"
 import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controller/postsController.js";
+import cors from "cors";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controller/postsController.js";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -18,6 +24,8 @@ const routes = (app) => {
     // falando para o express que ele vai usar json
     app.use(express.json()); 
 
+    app.use(cors(corsOptions))
+
     // rota para acessar a requisição no navegador usando o "/posts"
     app.get("/posts", listarPosts);
 
@@ -26,6 +34,9 @@ const routes = (app) => {
 
     // rota para inserir uma imagem
     app.post("/upload", upload.single("imagem"), uploadImagem)
+
+    // rota para atualizar um arquivo
+    app.put("/upload/:id", atualizarNovoPost)
 }
 
 export default routes;
